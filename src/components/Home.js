e6d5d40b1d0615adb56acbf8d0fdd55a69708f40
jsolano <justinesolano2370@gmail.com>
2021-05-08 18:21:40 +0100

    install bootstrap

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,15 @@
 import React from 'react'
-import ScrollIntoView from 'react-scroll-into-view'
+// import ScrollIntoView from 'react-scroll-into-view'
 import Header from './Header'
 import About from './About'
 
 // import disableScroll from 'disable-scroll'
 
-import about from '../assets/about.png'
-import contactme from '../assets/contactme.png'
-import experience from '../assets/experience.png'
-import projects from '../assets/projects.png'
-import skills from '../assets/skills.png'
+// import about from '../assets/about.png'
+// import contactme from '../assets/contactme.png'
+// import experience from '../assets/experience.png'
+// import projects from '../assets/projects.png'
+// import skills from '../assets/skills.png'
 
 
 const Home = () => {
@@ -19,15 +19,6 @@ const Home = () => {
 
   return (
     <>
-      <nav className="navbar">
-        <ScrollIntoView selector=".hr">
-          <img src={about} alt="about-me" className="nav-item"></img>
-        </ScrollIntoView>
-        <img src={skills} alt="skills" className="nav-item"></img>
-        <img src={experience} alt="experience" className="nav-item"></img>
-        <img src={projects} alt="project" className="nav-item"></img>
-        <img src={contactme} alt="contactme" className="nav-item"></img>
-      </nav>
       <div className="home">
         <Header className="header"/>
         <br />
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import ScrollIntoView from 'react-scroll-into-view'
+
 // import * as Scroll from 'react-scroll'
 // import { Link, Button, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
 
@@ -6,18 +8,30 @@ import about from '../assets/about.png'
 import contactme from '../assets/contactme.png'
 import experience from '../assets/experience.png'
 import projects from '../assets/projects.png'
-// import skills from '../assets/skills.png'
+import skills from '../assets/skills.png'
 
 const Nav = () => {
   return (
-    <nav className="navbar navbar-fixed-top">
-      <p><img src={about} alt="about" className="nav-item"></img></p>
-      {/* <p><img src={skills} alt="skills" className="nav-item"></img></p> */}
-      <p><img src={experience} alt="experience" className="nav-item"></img></p>
-      <p><img src={projects} alt="project" className="nav-item"></img></p>
-      <p><img src={contactme} alt="contactme" className="nav-item"></img></p>
+    <nav className="navbar">
+      <ScrollIntoView selector=".hr">
+        <img src={about} alt="about-me" className="nav-item"></img>
+      </ScrollIntoView>
+      <img src={skills} alt="skills" className="nav-item"></img>
+      <img src={experience} alt="experience" className="nav-item"></img>
+      <img src={projects} alt="project" className="nav-item"></img>
+      <img src={contactme} alt="contactme" className="nav-item"></img>
     </nav>
   )
 }
 
 export default Nav
+
+
+
+// <nav className="navbar navbar-fixed-top">
+//   <p><img src={about} alt="about" className="nav-item"></img></p>
+//   <p><img src={skills} alt="skills" className="nav-item"></img></p>
+//   <p><img src={experience} alt="experience" className="nav-item"></img></p>
+//   <p><img src={projects} alt="project" className="nav-item"></img></p>
+//   <p><img src={contactme} alt="contactme" className="nav-item"></img></p>
+// </nav>
